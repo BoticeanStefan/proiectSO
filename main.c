89dd91fb46c2e61acb7086907b6e234a819b6baa
BoticeanStefan <149998781+BoticeanStefan@users.noreply.github.com>
2023-11-19 18:30:22 +0200

    Update2 main.c

--- main.c
+++ main.c
@@ -7,14 +7,14 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <dirent.h>
-
+ 
 typedef struct {
     char signature[2];
     int size;
     int reserved;
     int offset;
 } BMPHeader;
-
+ 
 typedef struct {
     int header_size;
     int width;
@@ -29,66 +29,73 @@ typedef struct {
     int colors_important;
 } InfoHeader;
 
+typedef struct {
+    unsigned char b;
+    unsigned char g;
+    unsigned char r;
+} Pixel;
+
+
 void processFile(const char *filename, struct stat *file_info) {
     char statistic_info[256];
     int fd_output;
-
+ 
     char *file_type = "fisier obisnuit";
-
+ 
     if (S_ISLNK(file_info->st_mode)) {
         file_type = "legatura simbolica";
-
+ 
         char targetPath[256];
         ssize_t bytesRead = readlink(filename, targetPath, sizeof(targetPath) - 1);
         if (bytesRead != -1) {
             targetPath[bytesRead] = '\0';
             sprintf(statistic_info, "nume legatura: %s\ndimensiune legatura: %lld\ndimensiune fisier target: %lld\n",
                     filename, (long long)file_info->st_size, (long long)file_info->st_size);
-
+ 
 	    // DREPTURI DE ACCES
-
+ 
   // drepturi pentru user
   sprintf(statistic_info, "Drepturi de acces user:");
   write(fd_output, statistic_info, strlen(statistic_info));
-
+ 
   sprintf(statistic_info, (file_info->st_mode & S_IRUSR) ? "R" : "-");
   write(fd_output, statistic_info, strlen(statistic_info));
-
+ 
   sprintf(statistic_info, (file_info->st_mode & S_IWUSR) ? "W" : "-");
   write(fd_output, statistic_info, strlen(statistic_info));
-
+ 
   sprintf(statistic_info, (file_info->st_mode & S_IXUSR) ? "X\n" : "-\n");
   write(fd_output, statistic_info, strlen(statistic_info));
-
-  
+ 
+ 
   // drepturi pentru grup
   sprintf(statistic_info, "Drepturi de acces grup: ");
   write(fd_output, statistic_info, strlen(statistic_info));
-
+ 
   sprintf(statistic_info, (file_info->st_mode & S_IRGRP) ? "R" : "-");
   write(fd_output, statistic_info, strlen(statistic_info));
-
+ 
   sprintf(statistic_info, (file_info->st_mode & S_IWGRP) ? "W" : "-");
   write(fd_output, statistic_info, strlen(statistic_info));
-
+ 
   sprintf(statistic_info, (file_info->st_mode & S_IXGRP) ? "X\n" : "-\n");
   write(fd_output, statistic_info, strlen(statistic_info));
-
-  
-
+ 
+ 
+ 
   // drepturi pentru altii
   sprintf(statistic_info, "Drepturi de acces altii: ");
   write(fd_output, statistic_info, strlen(statistic_info));
-
+ 
   sprintf(statistic_info, (file_info->st_mode & S_IROTH) ? "R" : "-");
   write(fd_output, statistic_info, strlen(statistic_info));
-
+ 
   sprintf(statistic_info, (file_info->st_mode & S_IWOTH) ? "W" : "-");
   write(fd_output, statistic_info, strlen(statistic_info));
-
+ 
   sprintf(statistic_info, (file_info->st_mode & S_IXOTH) ? "X\n" : "-\n");
   write(fd_output, statistic_info, strlen(statistic_info));
-  
+ 
         } else {
             perror("readlink");
             exit(EXIT_FAILURE);
@@ -97,131 +104,194 @@ void processFile(const char *filename, struct stat *file_info) {
         file_type = "director";
         sprintf(statistic_info, "nume director: %s\nidentificatorul utilizatorului: %d\n",
                 filename, file_info->st_uid);
-  
+ 
         // Adaugam informatii despre fisierele din director
         DIR *dir;
         struct dirent *entry;
-
+ 
         dir = opendir(filename);
         if (dir == NULL) {
             perror("opendir");
             exit(EXIT_FAILURE);
         }
-
+ 
         while ((entry = readdir(dir)) != NULL) {
             if (strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0) {
                 char path[256];
                 snprintf(path, sizeof(path), "%s/%s", filename, entry->d_name);
-
+ 
                 struct stat entry_info;
                 if (lstat(path, &entry_info) == -1) {
                     perror("Eroare la primirea informatiilor");
                     exit(EXIT_FAILURE);
                 }
-
+ 
                 processFile(path, &entry_info);
-
-		
+ 
+ 
             }
         }
-
+ 
         closedir(dir);
     } else if (S_ISREG(file_info->st_mode)) {
         if (strstr(filename, ".bmp") != NULL) {
-
+ 
 	  int input_fd = open(filename, O_RDONLY);
-	  
+ 
 	    if(input_fd == -1){
 	      perror("EROARE la deschidere fisier BMP");
 	      exit(-1);
 	    }
-
+ 
 	    BMPHeader bmp_header;
  
-	    if(read(input_fd, &bmp_header, sizeof(BMPHeader)) == -1){
+	    if(read(input_fd, &bmp_header, sizeof(BMPHeader)-2) == -1){
 	      perror("EROARE citire BMP header");
 	      close(input_fd);
 	      exit(-1);
 	    }
-
+ 
 	    InfoHeader info_header;
-  
-	    if(read(input_fd, &info_header, sizeof(InfoHeader)) == -1){
+ 
+	    if(read(input_fd, &info_header, sizeof(InfoHeader)-4) == -1){
 	      perror("EROARE citire Info header");
 	      close(input_fd);
 	      exit(-1);
 	    }
-
+ 
 	    close(input_fd);
-	  
+ 
             sprintf(statistic_info, "nume fisier: %s\ninaltime: %d\nlungime: %d\ndimensiune: %lld\nidentificatorul utilizatorului: %d\ntimpul ultimei modificari: %s\n"
                                     "tipul fisierului: %s\ncontorul de legaturi: %d\n",
                     filename, info_header.height, info_header.width, (long long)file_info->st_size, file_info->st_uid, ctime(&(file_info->st_mtime)), file_type,
                     (int)file_info->st_nlink);//fisier cu .bmp
-        } else {
+        }
+
+pid_t grayProcess = fork();
+            if (grayProcess == -1) {
+                perror("EROARE la fork pentru conversia in tonuri de gri");
+                exit(EXIT_FAILURE);
+            }
+
+            if (grayProcess == 0) {
+                convertToGray(filename, "output_gray.bmp");
+                exit(EXIT_SUCCESS);
+            }
+	    
+	else {
             sprintf(statistic_info, "nume fisier: %s\ndimensiune: %lld\nidentificatorul utilizatorului: %d\ntimpul ultimei modificari: %s\n"
                                     "tipul fisierului: %s\ncontorul de legaturi: %d\n",
                     filename, (long long)file_info->st_size, file_info->st_uid, ctime(&(file_info->st_mtime)), file_type,
                     (int)file_info->st_nlink);//fisier fara .bmp
         }
     } else {
-        return; // Pentru orice alt caz, nu scriem nimic în fisier
+        return; // Pentru orice alt caz, nu scriem nimic in fisier
     }
-
+ 
     fd_output = open("statistica.txt", O_WRONLY | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR);
     if (fd_output == -1) {
         perror("EROARE la deschidere fisier output");
         exit(-1);
     }
-
+ 
     if (write(fd_output, statistic_info, strlen(statistic_info)) != strlen(statistic_info)) {
         perror("EROARE la scriere in fisier output");
         close(fd_output);
         exit(-1);
     }
-
+ 
     // DREPTURI DE ACCES
-
+ 
     // drepturi pentru user
     sprintf(statistic_info, "Drepturi de acces user:");
     write(fd_output, statistic_info, strlen(statistic_info));
-
+ 
     sprintf(statistic_info, (file_info->st_mode & S_IRUSR) ? "R" : "-");
     write(fd_output, statistic_info, strlen(statistic_info));
-
+ 
     sprintf(statistic_info, (file_info->st_mode & S_IWUSR) ? "W" : "-");
     write(fd_output, statistic_info, strlen(statistic_info));
-
+ 
     sprintf(statistic_info, (file_info->st_mode & S_IXUSR) ? "X\n" : "-\n");
     write(fd_output, statistic_info, strlen(statistic_info));
-
+ 
     // drepturi pentru grup
     sprintf(statistic_info, "Drepturi de acces grup: ");
     write(fd_output, statistic_info, strlen(statistic_info));
-
+ 
     sprintf(statistic_info, (file_info->st_mode & S_IRGRP) ? "R" : "-");
     write(fd_output, statistic_info, strlen(statistic_info));
-
+ 
     sprintf(statistic_info, (file_info->st_mode & S_IWGRP) ? "W" : "-");
     write(fd_output, statistic_info, strlen(statistic_info));
-
+ 
     sprintf(statistic_info, (file_info->st_mode & S_IXGRP) ? "X\n" : "-\n");
     write(fd_output, statistic_info, strlen(statistic_info));
-
+ 
     // drepturi pentru altii
     sprintf(statistic_info, "Drepturi de acces altii: ");
     write(fd_output, statistic_info, strlen(statistic_info));
-
+ 
     sprintf(statistic_info, (file_info->st_mode & S_IROTH) ? "R" : "-");
     write(fd_output, statistic_info, strlen(statistic_info));
-
+ 
     sprintf(statistic_info, (file_info->st_mode & S_IWOTH) ? "W" : "-");
     write(fd_output, statistic_info, strlen(statistic_info));
-
+ 
     sprintf(statistic_info, (file_info->st_mode & S_IXOTH) ? "X\n" : "-\n");
     write(fd_output, statistic_info, strlen(statistic_info));
-
+ 
     close(fd_output);
+}
+ 
+void convertToGray(const char *input_path, const char *output_path) {
+    int input_fd = open(input_path, O_RDONLY);
+    int output_fd = open(output_path, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+
+    if (input_fd == -1 || output_fd == -1) {
+        perror("EROARE la deschidere fisier");
+        exit(-1);
+    }
+
+    BMPHeader bmp_header;
+    InfoHeader info_header;
+
+    if (read(input_fd, &bmp_header, sizeof(BMPHeader)) == -1 || read(input_fd, &info_header, sizeof(InfoHeader)) == -1) {
+        perror("EROARE citire header BMP");
+        close(input_fd);
+        close(output_fd);
+        exit(-1);
+    }
+
+    Pixel pixel;
+
+    for (int y = 0; y < 1080; ++y) {
+        for (int x = 0; x < 1920; ++x) {
+            if (read(input_fd, &pixel, sizeof(Pixel)) == -1) {
+                perror("EROARE citire pixel");
+                close(input_fd);
+                close(output_fd);
+                exit(-1);
+            }
+
+            unsigned char gray = (unsigned char)(0.299 * pixel.r + 0.587 * pixel.g + 0.114 * pixel.b);
+
+            
+            pixel.r = gray;
+            pixel.g = gray;
+            pixel.b = gray;
+
+            if (write(output_fd, &pixel, sizeof(Pixel)) == -1) {
+                perror("EROARE scriere pixel gri");
+                close(input_fd);
+                close(output_fd);
+                exit(-1);
+            }
+        }
+    }
+
+    close(input_fd);
+    close(output_fd);
 }
 
 int main(int argc, char *argv[]) {
@@ -229,21 +299,32 @@ int main(int argc, char *argv[]) {
         perror("Usage ./program <fisier_intrare>");
         exit(-1);
     }
-
+ 
     int fd_output = open("statistica.txt", O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
     if (fd_output == -1) {
         perror("EROARE la deschidere fisier output");
         exit(-1);
     }
-    
+ 
     struct stat file_info;
-
+ 
     if (lstat(argv[1], &file_info) == -1) {
         perror("Eroare la primirea informatiilor");
         exit(EXIT_FAILURE);
     }
-
+ 
     processFile(argv[1], &file_info);
 
+    int status;
+    pid_t childPid;
+    while ((childPid = wait(&status)) != -1) {
+        printf("S-a incheiat procesul cu pid-ul %d si codul %d\n", childPid, status);
+    }
+    
+    // convertToGray(argv[1], "output_gray.bmp");
+ 
+ 
+ 
+ 
     return 0;
 }
